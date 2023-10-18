0b351bbbbe6af4a80ebd048ae42a956c811bfd8d
Najmi Aziz <najmiabo@gmail.com>
2023-10-18 23:19:53 +0700

    feat: add reviews CRUD

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,22 @@
 const Controller = require('../controller/controller')
+const authentication = require('../middlewares/authentication')
 
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('StudyBuddy is in da haaaussse')
 })
 
 router.post('/register', Controller.register)
 router.post('/login', Controller.login)
+
+router.use(authentication)
+
+router.patch('/users', Controller.updateRoleUser)
+
+router.post('/reviews/:projectId', Controller.createReview)
+router.get('/reviews', Controller.getReviews)
+router.delete('/reviews/:id', Controller.deleteReview)
+router.put('/reviews/:id', Controller.editReview)
 
 module.exports = router
