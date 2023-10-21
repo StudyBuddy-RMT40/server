3c84f9ffb726ebc7a1444a515b79f1ef3e88072c
kmassidik <k.massidik@gmail.com>
2023-10-22 00:14:39 +0700

    feat: implenet trasaction session in add project with add todos

--- test.js
+++ test.js
@@ -1,41 +1,22 @@
-[
-  {
-    _id: "6533cb7cd6a136e85dd3db46",
-    name: "test",
-    studentId: "65313e987ed17d8ee16f487a",
-    teacherId: "6533a8dcfb0b473524f9f4b4",
-    startDate: "2023-10-21T13:00:44.609Z",
-    endDate: null,
-    status: "submitted",
-    likes: 0,
-    description: "test test test",
-    categoryId: "653149fe502068bc912a8995",
-    published: false,
-    goals: "ap ya",
-    feedback: null,
-    category: {
-      _id: "653149fe502068bc912a8995",
-      name: "matematika",
-    },
+let data = [];
+let rawData =
+  '{\n  "title": "Pembuatan Kolam Ikan Lele",\n  "tasks": [\n    {\n      "task": "Membangun kolam ikan lele",\n      "priority": "High",\n      "due_date": "2022-05-01"\n    },\n    {\n      "task": "Menggali lubang kolam dengan dimensi 4x6 meter",\n      "priority": "Medium",\n      "due_date": "2022-05-05"\n    },\n    {\n      "task": "Membuat kerangka kolam menggunakan kayu atau besi",\n      "priority": "Medium",\n      "due_date": "2022-05-08"\n    },\n    {\n      "task": "Memasang liner plastik sebagai penahan air",\n      "priority": "High",\n      "due_date": "2022-05-10"\n    },\n    {\n      "task": "Mengisi kolam dengan air bersih",\n      "priority": "High",\n      "due_date": "2022-05-12"\n    },\n    {\n      "task": "Mengatur sistem sirkulasi air dan filter",\n      "priority": "High",\n      "due_date": "2022-05-15"\n    },\n    {\n      "task": "Menanam rumput di sekitar kolam",\n      "priority": "Low",\n      "due_date": "2022-05-20"\n    },\n    {\n      "task": "Menambahkan pupuk organik untuk pertumbuhan alga",\n      "priority": "Medium",\n      "due_date": "2022-05-22"\n    },\n    {\n      "task": "Memasukkan bibit ikan lele ke dalam kolam",\n      "priority": "High",\n      "due_date": "2022-05-25"\n    },\n    {\n      "task": "Memberi pakan ikan lele secara teratur",\n      "priority": "High",\n      "due_date": "2022-05-28"\n    },\n    {\n      "task": "Memastikan kualitas air kolam tetap optimal",\n      "priority": "High",\n      "due_date": "2022-06-01"\n    },\n    {\n      "task": "Melakukan pemeliharaan dan pembersihan kolam secara berkala",\n      "priority": "Medium",\n      "due_date": "2022-06-05"\n    }\n  ]\n}';
+let parsedData = JSON.parse(rawData);
+console.log(parsedData);
+data.push({
+  title: parsedData.title,
+  tasks: parsedData.tasks.map((task) => ({
+    task: task.task,
+    priority: task.priority,
+    due_date: task.due_date,
+  })),
+});
 
-    teacher: {
-      _id: "6533a8dcfb0b473524f9f4b4",
-      username: "buddy",
-      email: "buddy@buddy.com",
-      password: "$2a$10$Tyv6JYeZRPjwl8pliZLXzufiiEqtZr2eDsXzA2ZZvAamvoFe4n3Bu",
-      phoneNumber: null,
-      role: "teacher",
-      address: null,
-    },
+console.log(data);
+console.log(data[0].title); // Outputs: 'Pembuatan Kolam Ikan Lele'
 
-    student: {
-      _id: "65313e987ed17d8ee16f487a",
-      username: "admin",
-      email: "admin@admin.com",
-      password: "$2a$10$T5lmAA1QKLgCWcru1H/.ZO9SLAu9cBho2TeIS0.Bb3Eu1b0yXujPi",
-      phoneNumber: "123123",
-      role: "student",
-      address: "bekasi",
-    },
-  },
-];
+data[0].tasks.forEach((task) => {
+  console.log("Task: " + task.task);
+  console.log("Priority: " + task.priority);
+  console.log("Due Date: " + task.due_date);
+});
