2a0f9d9378d354e927e501c798370d69ae57b1c5
kmassidik <k.massidik@gmail.com>
2023-10-24 00:29:35 +0700

    fix: fixing mongo client close

--- services/mongo/models/storage.js
+++ services/mongo/models/storage.js
@@ -10,27 +10,6 @@ class Storage {
     const newMediaUrl = await this.storageCollection().insertOne(data);
     return newMediaUrl;
   }
-
-  // static async findOneAndUpdate(id, updateObject) {
-  //   const result = await this.storageCollection().findOneAndUpdate(
-  //     { _id: new ObjectId(id) }, 
-  //     { $set: updateObject }, 
-  //     { returnOriginal: false } 
-  //   );
-  //   return result
-  // }
-
-  static async findAll() {
-    const mediaUrl = await this.storageCollection().find().toArray();
-    return mediaUrl;
-  }
-
-  static async findById(id) {
-    const mediaUrl = await this.storageCollection().findOne({
-      projectId: new ObjectId(id),
-    });
-    return mediaUrl;
-  }
 }
 
 module.exports = Storage;
